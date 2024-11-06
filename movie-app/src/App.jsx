a0db89025d2807a7f59ebc65ced130ab648078f4
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-11-06 14:31:25 +0700

    create project

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React ,{ useState } from 'react'
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=e941bffc';
+
+function App() {
+  
+
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default App
